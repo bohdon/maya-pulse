302d27f94e59a313029fe0112ce6389e685df059
Bohdon Sayre <bohdon@gmail.com>
2019-02-19 02:03:08 -0500

    changes space switches to simpler and faster utility chain

--- src/pulse/scripts/pulse/spaces.py
+++ src/pulse/scripts/pulse/spaces.py
@@ -1,3 +1,17 @@
+"""
+Space Switching Constraint Setup
+    - Store offsets from each space node in new attrs on the follower
+        (follower.wm * space.wm.inverse)
+    - Connect all offset matrices to 'offsetChoice' (choice) node
+    - Connect all space node world matrices to 'spaceChoice' (choice) node
+    - Connect 'space' attribute (enum) to selector of both choice nodes
+    - Connect output of choices to calc new follower world matrix (multMatrix)
+        (offsetChoice.output * spaceChoice.output * follower.pim)
+    - Decompose and connect to follower trs
+
+(see space switching solution by Jarred Love)
+
+"""
 
 import os
 import logging
@@ -96,66 +110,72 @@ def prepareSpaceConstraint(node, follower, spaceNames):
 
     Args:
         node (PyNode): The node that will contain space switching attrs
-        follower (PyNode): The node that will be constrained, can be
-            different than the control.
+        follower (PyNode): The node that will be constrained, can be `node`,
+            or more commonly, a parent (offset) of `node`.
         spaceNames (str list): The names of all spaces to be applied
     """
-
-    data = {
-        # native spaces in this constraint
-        'spaces': [],
-        # dynamic spaces (added during animation), which may be
-        # from the native rig, or from an external one
-        'dynamicSpaces': [],
-        # transform that is actually constrained
-        'constrainedNode': follower,
-        # the addition utility nodes internal to the constraint
-        'translateAdd': None,
-        'rotateAdd': None,
-        'scaleAdd': None,
-    }
-
-    # native space indeces always take priority,
-    # which means dynamic spaces may be adversely affected
-    # if the native spaces change on a published rig
-    # TODO: ability to reindex dynamic spaces
-    for i, spaceName in enumerate(spaceNames):
-        data['spaces'].append({
-            'name': spaceName,
-            'switch': None,
-            'index': i,
-        })
-
-    # create addition utilities for transform attrs
-    addUtilities = [
-        ('t', 'translateAdd'),
-        ('r', 'rotateAdd'),
-        ('s', 'scaleAdd'),
-    ]
-
-    for attrName, metaKey in addUtilities:
-        # create addition utility
-        defaultVal = 1 if (attrName == 's') else 0
-        # create add node with first item set to defaults
-        addNode = pulse.utilnodes.add(
-            (defaultVal, defaultVal, defaultVal)).node()
-        addNode.rename('{0}_add{1}'.format(
-            node.nodeName(), attrName.upper()))
-
-        # connect to the follower node
-        addNode.output3D >> follower.attr(attrName)
-
-        # store reference to utility in meta data
-        data[metaKey] = addNode
-
-    meta.setMetaData(node, SPACECONSTRAINT_METACLASS, data)
 
     # setup space switching attr
     if not node.hasAttr(SPACESWITCH_ATTR):
         enumNames = ':'.join(spaceNames)
         node.addAttr(SPACESWITCH_ATTR, at='enum', en=enumNames)
-        sattr = node.attr(SPACESWITCH_ATTR)
-        sattr.setKeyable(True)
+        spaceAttr = node.attr(SPACESWITCH_ATTR)
+        spaceAttr.setKeyable(True)
+    else:
+        spaceAttr = node.attr(SPACESWITCH_ATTR)
+
+    nodeName = node.nodeName()
+
+    offsetChoiceName = nodeName + '_space_offset_choice'
+    spaceChoiceName = nodeName + '_space_choice'
+    multMatrixName = nodeName + '_space_mmtx'
+    decompName = nodeName + '_space_decomp'
+
+    # create utility nodes
+    offsetChoice = pm.shadingNode('choice', n=offsetChoiceName, asUtility=True)
+    spaceChoice = pm.shadingNode('choice', n=spaceChoiceName, asUtility=True)
+    multMatrix = pm.shadingNode('multMatrix', n=multMatrixName, asUtility=True)
+    decomp = pm.shadingNode('decomposeMatrix', n=decompName, asUtility=True)
+
+    # setup connections
+    spaceAttr >> offsetChoice.selector
+    spaceAttr >> spaceChoice.selector
+    offsetChoice.output >> multMatrix.matrixIn[0]
+    spaceChoice.output >> multMatrix.matrixIn[1]
+    follower.pim >> multMatrix.matrixIn[2]
+    multMatrix.matrixSum >> decomp.inputMatrix
+    # connection from decomp output to the follower trs
+    # occurs after the actual space nodes are connected
+
+    spaceData = []
+    # native space indeces always take priority,
+    # which means dynamic spaces may be adversely affected
+    # if the native spaces change on a published rig
+    # TODO: ability to reindex dynamic spaces
+    for i, spaceName in enumerate(spaceNames):
+        spaceData.append({
+            'name': spaceName,
+            # TODO: is `switch` needed anymore?
+            'switch': None,
+            'index': i,
+        })
+
+    data = {
+        # native spaces in this constraint
+        'spaces': spaceData,
+        # dynamic spaces (added during animation), which may be
+        # from the native rig, or from an external one
+        'dynamicSpaces': [],
+        # transform that is actually driven by the space constraint
+        'follower': follower,
+        # the utility nodes that make up the space constraint
+        'offsetChoice': offsetChoice,
+        'spaceChoice': spaceChoice,
+        'multMatrix': multMatrix,
+        'decompose': decomp,
+    }
+
+    meta.setMetaData(node, SPACECONSTRAINT_METACLASS, data)
 
 
 def createSpaceConstraints(nodes):
@@ -190,8 +210,8 @@ def createSpaceConstraint(node):
 
 def _createSpaceConstraint(node, spaceNodesByName):
     """
-    Create the actual constraints for each defined space in
-    a space constraint node.
+    Connect all spaces defined in the node's space data
+    to the space constraint utility nodes.
 
     Args:
         node (PyNode): The space constraint node
@@ -199,76 +219,56 @@ def _createSpaceConstraint(node, spaceNodesByName):
             nodes index by name.
     """
     data = meta.getMetaData(node, SPACECONSTRAINT_METACLASS)
+    didConnectAny = False
     for spaceData in data['spaces']:
+        index = spaceData['index']
         # ensure the switch is not already created
         if not spaceData['switch']:
             spaceNode = spaceNodesByName.get(spaceData['name'], None)
             if spaceNode:
-                _createConstraintSwitchForSpace(
-                    node, data, spaceData, spaceNode)
+                _connectSpaceToConstraint(node, data, index, spaceNode)
+                didConnectAny = True
             else:
                 LOG.warning(
                     "Space node not found: {0}".format(spaceData['name']))
 
+    if didConnectAny:
+        # connect space constraint output now that at least one space is setup
+        # TODO: make sure the space 0 is setup, or whatever the attrs value is
+        decomp = data['decompose']
+        follower = data['follower']
+        decomp.outputTranslate >> follower.translate
+        decomp.outputRotate >> follower.rotate
+        decomp.outputScale >> follower.scale
 
-def _createConstraintSwitchForSpace(node, spaceConstraintData, spaceData, spaceNode):
+
+def _connectSpaceToConstraint(node, spaceConstraintData, index, spaceNode):
     """
-    Create and connect the conditions and constraints for a space constraint
-    connected to a specific space node.
+    Calculate and store the offset for a space, and connect the space
+    to the choice nodes of a space constraint.
 
     Args:
         node (PyNode): The space constraint node
         spaceConstraintData (dict): The loaded space constraint data
             from the space constraint node
-        spaceData (dict): The dict containing space data for the
-            constraint, set during `prepareSpaceConstraint` or
-            `addDynamicSpace`
+        index (int): The index of the space being connected
         spaceNode (PyNode): The node representing the space
     """
-    spaceAttr = node.attr(SPACESWITCH_ATTR)
-    index = spaceData['index']
-    nodeName = node.nodeName()
+    # calculate and store the offset between the space and follower
+    follower = spaceConstraintData['follower']
+    offsetMatrix = follower.wm.get() * spaceNode.wm.get().inverse()
+    offsetAttrName = 'space{}_offset'.format(index)
+    follower.addAttr(offsetAttrName, dt='matrix')
+    offsetAttr = follower.attr(offsetAttrName)
+    offsetAttr.set(offsetMatrix)
 
-    # create the condition that will control whether
-    # the constraints from this space are be active
-    # node state 0 == active, 2 == disabled
-    stateCndAttr = pulse.utilnodes.equal(spaceAttr, index, 0, 2)
-    stateCndNode = stateCndAttr.node()
-    stateCndNode.rename('{0}_isSpace{1}Active'.format(nodeName, index))
+    # connect offset choice
+    offsetChoice = spaceConstraintData['offsetChoice']
+    offsetAttr >> offsetChoice.input[index]
 
-    # create parent and scale constraints
-    follower = spaceConstraintData['constrainedNode']
-    pc = createPartialParentConstraint(spaceNode, follower)
-    pc.rename('space{0}_parentConstraint'.format(index))
-    pm.parentConstraint(spaceNode, pc, e=True, mo=True)
-    sc = createPartialScaleConstraint(spaceNode, follower)
-    sc.rename('space{0}_scaleConstraints'.format(index))
-    for c in (pc, sc):
-        stateCndAttr >> c.nodeState
-
-        # TODO: don't lock dynamic space constraints
-        pulse.nodes.setConstraintLocked(c, True)
-
-    # create value conditions for each attribute
-    valTCndAttr = pulse.utilnodes.equal(
-        spaceAttr, index, pc.constraintTranslate, (0, 0, 0))
-    valRCndAttr = pulse.utilnodes.equal(
-        spaceAttr, index, pc.constraintRotate, (0, 0, 0))
-    valSCndAttr = pulse.utilnodes.equal(
-        spaceAttr, index, sc.constraintScale, (0, 0, 0))
-    valTCndAttr.node().rename('{0}_space{1}_TVal'.format(nodeName, index))
-    valRCndAttr.node().rename('{0}_space{1}_RVal'.format(nodeName, index))
-    valSCndAttr.node().rename('{0}_space{1}_SVal'.format(nodeName, index))
-
-    # connect to addition nodes
-    addTNode = spaceConstraintData['translateAdd']
-    addRNode = spaceConstraintData['rotateAdd']
-    addSNode = spaceConstraintData['scaleAdd']
-    valTCndAttr >> addTNode.input3D[index]
-    valRCndAttr >> addRNode.input3D[index]
-    valSCndAttr >> addSNode.input3D[index]
-
-    return stateCndNode
+    # connect space choice
+    spaceChoice = spaceConstraintData['spaceChoice']
+    spaceNode.wm >> spaceChoice.input[index]
 
 
 def _getAllSpacesInConstraint(spaceConstraintData):
@@ -310,97 +310,3 @@ def addDynamicSpace(node, space):
     to setup the native spaces of the constraint.
     """
     pass
-
-
-def createPartialParentConstraint(leader, follower):
-    """
-    Create a parent constraint node between a leader and follower,
-    but do not connect it to the follower node's transform attrs,
-    only connect inputs from the two nodes into the constraint.
-
-    Args:
-        leader (PyNode): The node that will lead the constraint
-        follower (PyNode): The node affected by the constraint
-    """
-
-    # create the constraint node
-    con = pm.createNode('parentConstraint')
-    con.setParent(follower)
-
-    # hiding the constraint prevents camera framing issues if the pivot location is off
-    con.visibility.set(0)
-
-    # connect inputs from leader
-    leadConnections = {
-        't': 'target[0].targetTranslate',
-        'r': 'target[0].targetRotate',
-        's': 'target[0].targetScale',
-        'ro': 'target[0].targetRotateOrder',
-        'rp': 'target[0].targetRotatePivot',
-        'rpt': 'target[0].targetRotateTranslate',
-        'pm[0]': 'target[0].targetParentMatrix',
-    }
-    if isinstance(leader, pm.nt.Joint):
-        leadConnections['jo'] = 'target[0].targetJointOrient'
-
-    for srcname, dstname in leadConnections.items():
-        src = leader.attr(srcname)
-        dst = con.attr(dstname)
-        src >> dst
-
-    # connect inputs from constrained node
-    followerConnections = {
-        'ro': 'constraintRotateOrder',
-        'rp': 'constraintRotatePivot',
-        'rpt': 'constraintRotateTranslate',
-        'pim[0]': 'constraintParentInverseMatrix',
-    }
-
-    for srcname, dstname in followerConnections.items():
-        src = follower.attr(srcname)
-        dst = con.attr(dstname)
-        src >> dst
-
-    return con
-
-
-def createPartialScaleConstraint(leader, follower):
-    """
-    Create a scale constraint node between a leader and follower,
-    but do not connect it to the follower node's transform attrs,
-    only connect inputs from the two nodes into the constraint.
-
-    Args:
-        leader (PyNode): The node that will lead the constraint
-        follower (PyNode): The node affected by the constraint
-    """
-
-    # create the constraint
-    con = pm.createNode('scaleConstraint')
-    con.setParent(follower)
-
-    # hiding the constraint prevents camera framing issues if the pivot location is off
-    con.visibility.set(0)
-
-    # connect inputs from leader
-    leaderConnections = {
-        's': 'target[0].targetScale',
-        'pm[0]': 'target[0].targetParentMatrix',
-    }
-
-    for srcname, dstname in leaderConnections.items():
-        src = leader.attr(srcname)
-        dst = con.attr(dstname)
-        src >> dst
-
-    # connect inputs from constrained node
-    followerConnections = {
-        'pim[0]': 'constraintParentInverseMatrix',
-    }
-
-    for srcname, dstname in followerConnections.items():
-        src = follower.attr(srcname)
-        dst = con.attr(dstname)
-        src >> dst
-
-    return con
