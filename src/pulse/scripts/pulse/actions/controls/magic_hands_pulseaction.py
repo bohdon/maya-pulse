91b5f3252a15c0b023490901970db545f72871c7
Bohdon Sayre <bohdon@gmail.com>
2020-04-27 17:44:43 -0400

    add magic hands action

---
+++ src/pulse/scripts/pulse/actions/controls/magic_hands_pulseaction.py
@@ -0,0 +1,62 @@
+
+import pymel.core as pm
+import pymetanode as meta
+
+import pulse
+import pulse.nodes
+
+
+class MagicHandsAction(pulse.BuildAction):
+
+    _offsetName = '{0}_magic'
+
+    def validate(self):
+        if not self.ctl1 and not self.ctl2 and not self.ctl3 and not self.ctl4:
+            raise pulse.BuildActionError("No controls were given")
+
+    def run(self):
+        fingerCtls = [self.ctl2, self.ctl3, self.ctl4]
+        fingerCtls = [c for c in fingerCtls if c]
+
+        # TODO(bsayre): reuse these attributes across multiple fingers
+        # convert finger position from 0..1, to 1..-1
+        splayFactor = (self.fingerPosition - 0.5) * 2.0
+        # get a zeroed scaleY attr for driving splay
+        syZeroed = pulse.utilnodes.add(self.control.sy, -1)
+
+        if self.ctl1:
+            # {metacarpal}.rz = (sy - 1) * splayFactor * splayScale * 0.5
+            metacarpalRz = pulse.utilnodes.multiply(
+                syZeroed, splayFactor * self.splayScale * 0.5)
+
+            # create and connect offset
+            offset1 = pulse.nodes.createOffsetTransform(
+                self.ctl1, name=self._offsetName)
+            metacarpalRz >> offset1.rz
+
+        # TODO(bsayre): Expose scalar for rotation-splay?
+        splayAttr = pulse.utilnodes.multiply(
+            self.control.rx, splayFactor * -1.0)
+
+        # {finger.ry} = ry + (rx * splayFactor)
+        fingersRy = pulse.utilnodes.add(
+            self.control.ry, splayAttr)
+        # for first joint of a finger, add rotate offset driven by translate
+        # {finger}.ry = tz * metaRotateScale
+        tzScaled = pulse.utilnodes.multiply(
+            self.control.tz, self.metaRotateScale)
+        baseFingersRy = pulse.utilnodes.add(
+            fingersRy, tzScaled)
+        # {finger.rz} = (sy - 1) * splayFactor * splayScale
+        fingersRz = pulse.utilnodes.multiply(
+            syZeroed, splayFactor * self.splayScale)
+
+        for i, ctl in enumerate(fingerCtls):
+            offset = pulse.nodes.createOffsetTransform(
+                ctl, name=self._offsetName)
+            if i == 0:
+                # create and connect offsets
+                baseFingersRy >> offset.ry
+            else:
+                fingersRy >> offset.ry
+            fingersRz >> offset.rz
