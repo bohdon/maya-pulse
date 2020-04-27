de344768317852b31c9cdb6408013efd7cf0c0de
Bohdon Sayre <bohdon@gmail.com>
2020-04-27 14:40:59 -0400

    implement aim constrain blend

--- src/pulse/scripts/pulse/actions/constraints/aimconstrain_pulseaction.py
+++ src/pulse/scripts/pulse/actions/constraints/aimconstrain_pulseaction.py
@@ -29,20 +29,45 @@ class AimConstrainAction(pulse.BuildAction):
         if shouldCreateOffset:
             _follower = pulse.nodes.createOffsetTransform(self.follower)
 
+        # create blend
+        if self.createBlend:
+            # create 2 more transforms to represent aiming off/on
+            aimOffNode = pm.group(empty=True, p=_follower,
+                                  name='%s_aimOff' % self.follower.nodeName())
+            aimOnNode = pm.group(empty=True, p=_follower,
+                                 name='%s_aimOn' % self.follower.nodeName())
+
+            # move new nodes so they are siblings
+            # (originally parented to follower to inherit the same transform)
+            aimOffNode.setParent(_follower.getParent())
+            aimOnNode.setParent(_follower.getParent())
+
+            # create matrix blend between off and on
+            wtAdd = pm.createNode('wtAddMatrix')
+            aimOnNode.worldMatrix >> wtAdd.wtMatrix[0].matrixIn
+            aimOffNode.worldMatrix >> wtAdd.wtMatrix[1].matrixIn
+            pulse.utilnodes.connectMatrix(wtAdd.matrixSum, _follower)
+
+            # create blend attr and connect to matrix blend
+            self.follower.addAttr("aimBlend", min=0, max=1, at='double',
+                                  defaultValue=1, keyable=1)
+            blendAttr = self.follower.attr("aimBlend")
+            blendAttr >> wtAdd.wtMatrix[0].weightIn
+            pulse.utilnodes.reverse(blendAttr) >> wtAdd.wtMatrix[1].weightIn
+
+            # use aimOn node as new follower for the aim constraint
+            _follower = aimOnNode
+
         # create aim constrain
         upTypeStr = [
             'objectrotation',
             'object',
         ][self.worldUpType]
         ac = pm.aimConstraint(
-            self.leader, _follower, mo=True,
+            self.leader, _follower, mo=False,
             aimVector=self.aimVector, upVector=self.upVector,
             worldUpType=upTypeStr, worldUpObject=self.worldUpObject,
             worldUpVector=self.worldUpVector)
 
         # lockup the constraints
         pulse.nodes.setConstraintLocked(ac, True)
-
-        # create blend
-        if self.createBlend:
-            pass
