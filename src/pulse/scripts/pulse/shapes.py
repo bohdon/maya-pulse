a5916043de1b9b71ab1762b742d7e679eae01dda
Bohdon Sayre <bohdon@gmail.com>
2020-05-18 17:38:47 -0400

    add combineShapes util

--- src/pulse/scripts/pulse/shapes.py
+++ src/pulse/scripts/pulse/shapes.py
@@ -27,3 +27,33 @@ def rotateComponents(shape, rotation):
         pm.rotate(shape.cv, rotation, objectSpace=True)
     else:
         LOG.error("{0} is not a valid shape".format(shape))
+
+
+def combineShapes(transforms):
+    """
+    Combine multiple shape nodes into one transform with all the shapes.
+
+    Args:
+        transforms: A list of transform nodes with shapes to combine
+
+    Returns:
+        The combined transform node containing all the shapes
+    """
+    # get first transform, and all shapes
+    target = transforms[0]
+    shapes = []
+    for t in transforms:
+        shapes.extend(t.getShapes())
+
+    # reparent shapes to first transform
+    for shape in shapes:
+        pm.parent(shape, target, shape=True, relative=True)
+
+    # delete empty remaining transforms
+    nodesToDelete = []
+    for t in transforms:
+        if not t.getChildren():
+            nodesToDelete.append(t)
+    pm.delete(nodesToDelete)
+
+    return target
