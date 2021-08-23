8fbb0ab4fe95d6478854be0d553054106c6ecc9d
Bohdon Sayre <bohdon@gmail.com>
2021-08-23 17:58:38 -0400

    add space switching context menu

--- src/pulse/scripts/pulse/actions/constraints/spaces_pulseaction.py
+++ src/pulse/scripts/pulse/actions/constraints/spaces_pulseaction.py
@@ -1,6 +1,10 @@
+import pymel.core as pm
+
 import pulse.nodes
 import pulse.spaces
+import pymetanode as meta
 from pulse.buildItems import BuildAction, BuildActionError
+from pulse.views.contextmenus import PulseNodeContextSubMenu
 
 
 class CreateSpaceAction(BuildAction):
@@ -49,3 +53,59 @@ class ApplySpacesAction(BuildAction):
         # TODO: only gather not-yet-created constraints
         allConstraints = pulse.spaces.getAllSpaceConstraints()
         pulse.spaces.connectSpaceConstraints(allConstraints)
+
+
+class SpaceSwitchUtils(object):
+    @staticmethod
+    def switchSpace(ctl: pm.PyNode, space: str) -> bool:
+        """
+        Switch a control into a new space
+
+        Args:
+            ctl: A node with space switching meta data
+            space: The name of the space to switch to
+
+        Returns:
+            True if the space was changed, false otherwise
+        """
+        meta_data = meta.getMetaData(ctl, pulse.spaces.SPACECONSTRAINT_METACLASS)
+        space_data = [s for s in meta_data.get('spaces', []) if s['name'] == space]
+        if not space_data:
+            return False
+
+        space_data = space_data[0]
+        index = space_data['index']
+
+        # remember world matrix
+        wm = ctl.wm.get()
+        # change space
+        ctl.attr('space').set(index)
+        # restore world matrix
+        pulse.nodes.setWorldMatrix(ctl, wm)
+
+        return True
+
+
+class SpaceSwitchContextSubMenu(PulseNodeContextSubMenu):
+
+    @classmethod
+    def shouldBuildSubMenu(cls, menu) -> bool:
+        # TODO: support switching multiple nodes if they have overlapping spaces
+        return len(cls.getSelectedNodesWithMetaClass(pulse.spaces.SPACECONSTRAINT_METACLASS)) == 1
+
+    def buildMenuItems(self):
+        ctl = self.getSelectedNodesWithMetaClass(pulse.spaces.SPACECONSTRAINT_METACLASS)[0]
+        meta_data = meta.getMetaData(ctl, pulse.spaces.SPACECONSTRAINT_METACLASS)
+        spaces = meta_data.get('spaces', [])
+        if spaces:
+            pm.menuItem(l="Spaces", enable=False)
+            for space in spaces:
+                index = space['index']
+                is_current = ctl.attr('space').get() == index
+
+                title = pulse.names.toTitle(space['name'])
+                suffix = ' (Default)' if index == 0 else ''
+                prefix = '> ' if is_current else '    '
+                display_name = f"{prefix}{title}{suffix}"
+
+                pm.menuItem(l=display_name, c=pm.Callback(SpaceSwitchUtils.switchSpace, ctl, space['name']))
