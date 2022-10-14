2d115e84452e4e77e85d9a7552cba92fb9df9020
Bohdon Sayre <bohdon@gmail.com>
2022-10-14 14:43:33 -0400

    move actions to builtin_actions sub package

---
+++ src/pulse/scripts/pulse/builtin_actions/__init__.py
@@ -0,0 +1,8 @@
+"""
+The built-in Pulse actions package.
+
+Action modules are not connected to this package, but are instead imported individually and dynamically.
+This allows more easily adding new actions without needing to maintain the python package.
+
+The package itself is simply used to locate the directory where the actions can be found.
+"""
