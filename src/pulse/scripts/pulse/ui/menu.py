e992a12bf1de149c4d6d2c1346df4973a86a9397
Bohdon Sayre <bohdon@gmail.com>
2022-10-28 04:11:04 -0400

    add help and other editors to main menu

--- src/pulse/scripts/pulse/ui/menu.py
+++ src/pulse/scripts/pulse/ui/menu.py
@@ -1,10 +1,14 @@
 """
 Menus that can be installed into the Maya UI for quickly launching Pulse.
 """
+import webbrowser
+
 import pymel.core as pm
 
 MAIN_MENU_LABEL = 'Pulse'
 MAIN_MENU_ID = 'pulse_main_menu'
+DOCUMENTATION_URL = 'https://maya-pulse.readthedocs.io/'
+ISSUES_URL = 'https://github.com/bohdon/maya-pulse/issues'
 
 
 def _cmd(command):
@@ -18,6 +22,13 @@ def _cmd(command):
     return _callback
 
 
+def _url_cmd(url):
+    def _callback(*args, **kwargs):
+        webbrowser.open(url)
+
+    return _callback
+
+
 def install_main_menu():
     """
     Install the top-level Pulse menu.
@@ -25,6 +36,8 @@ def install_main_menu():
     from .. import reload
     from . import toggle_editor_ui
     from .actioneditor import ActionEditorWindow
+    from .actionpalette import ActionPaletteWindow
+    from .actiontree import ActionTreeWindow
     from .designtoolkit import DesignToolkitWindow
     from .main_settings import MainSettingsWindow
 
@@ -34,12 +47,18 @@ def install_main_menu():
     pm.menuItem(parent=MAIN_MENU_ID, label="Pulse Editor", command=_cmd(toggle_editor_ui))
     pm.menuItem(parent=MAIN_MENU_ID, label="Design Toolkit", command=_cmd(DesignToolkitWindow.toggleWindow))
     pm.menuItem(parent=MAIN_MENU_ID, label="Action Editor", command=_cmd(ActionEditorWindow.toggleWindow))
+    pm.menuItem(parent=MAIN_MENU_ID, label="Action Palette", command=_cmd(ActionPaletteWindow.toggleWindow))
+    pm.menuItem(parent=MAIN_MENU_ID, label="Action Tree", command=_cmd(ActionTreeWindow.toggleWindow))
 
     pm.menuItem(parent=MAIN_MENU_ID, label="Settings", divider=True)
     pm.menuItem(parent=MAIN_MENU_ID, label="Settings", command=_cmd(MainSettingsWindow.toggleWindow))
 
     pm.menuItem(parent=MAIN_MENU_ID, label="Utils", divider=True)
     pm.menuItem(parent=MAIN_MENU_ID, label="Reload", command=_cmd(reload))
+
+    pm.menuItem(parent=MAIN_MENU_ID, label="Help", divider=True)
+    pm.menuItem(parent=MAIN_MENU_ID, label="Documentation", command=_url_cmd(DOCUMENTATION_URL))
+    pm.menuItem(parent=MAIN_MENU_ID, label="Report an Issue...", command=_url_cmd(ISSUES_URL))
 
 
 def _create_menu(label: str, menu_id: str):
