597c49bc744199bc5ceb8b06dbabe5c45852dabe
Bohdon Sayre <bohdon@gmail.com>
2022-11-15 00:28:19 -0500

    add pascal case name util

--- src/pulse/scripts/pulse/names.py
+++ src/pulse/scripts/pulse/names.py
@@ -1,16 +1,25 @@
+"""
+Utils for working with naming conventions.
+"""
+
 import re
 
 
-def to_title(name):
+def to_title(name: str) -> str:
     """
     Return a string formatted to 'Title Case'
     """
-    if not isinstance(name, str):
-        return
     return re.sub("([A-Z])", " \\1", name).title()
 
 
-def to_camel(name):
+def to_pascal(name: str) -> str:
+    """
+    Return a string formatted to 'PascalCase'
+    """
+    return to_title(name).replace(" ", "")
+
+
+def to_camel(name: str) -> str:
     """
     Return a string formatted to 'camelCase'
     """
