71f1900620d3621d5227529f00cd2f20d2a5c0d2
Bohdon Sayre <bohdon@gmail.com>
2023-02-20 11:52:33 -0500

    add fk support to spline ik

--- src/pulse/scripts/pulse/builtin_actions/kinematics/spline_ikfk.py
+++ src/pulse/scripts/pulse/builtin_actions/kinematics/spline_ikfk.py
@@ -46,11 +46,16 @@ class SplineIKFKAction(BuildAction):
             description="The end control in IK mode.",
         ),
         dict(
+            name="fkCtls",
+            type=AttrType.NODE_LIST,
+            description="The ctls to use for each joint in FK mode.",
+        ),
+        dict(
             name="isStretchy",
             type=AttrType.BOOL,
             value=True,
             description="Enable stretchiness by translating bones based on the spline length.",
-        )
+        ),
     ]
 
     def validate(self):
@@ -63,7 +68,37 @@ class SplineIKFKAction(BuildAction):
         start_jnt = self.startJoint
         end_jnt = self.endJoint
         mid_jnts = self._get_joint_chain(start_jnt, end_jnt)
-        target_jnts = [start_jnt] + mid_jnts[:] + [end_jnt]
+        target_jnts: List[pm.nt.Joint] = [start_jnt] + mid_jnts[:] + [end_jnt]
+        self.logger.debug(f"target_jnts: {target_jnts}")
+
+        # setup ik chain
+        ik_jnts = self._setup_ik_chain(start_jnt, end_jnt)
+        ik_ctls = [self.endIkCtl] + self.midIkCtls
+        # use the end ik control directly when connecting to end joint so that rotation is explicit
+        ik_leaders = ik_jnts[:-1] + [self.endIkCtl]
+        self.logger.debug(f"ik_leaders: {ik_leaders}")
+
+        # setup fk chain
+        # include root ctl as one of the fk controls
+        fk_ctls = self._get_closest_ctls(target_jnts, [self.startCtl] + self.fkCtls)
+        self.logger.debug(f"fk_ctls: {fk_ctls}")
+
+        ik_attr = self._setup_ikfk_switch(target_jnts, ik_leaders, fk_ctls)
+
+        # connect visibility
+        if not self.builder.debug:
+            for ik_ctl in ik_ctls:
+                ik_ctl.v.setLocked(False)
+                ik_attr >> ik_ctl.v
+                ik_ctl.v.setLocked(True)
+
+            fk_attr = util_nodes.reverse(ik_attr)
+            for fk_ctl in self.fkCtls:
+                fk_ctl.v.setLocked(False)
+                fk_attr >> fk_ctl.v
+                fk_ctl.v.setLocked(True)
+
+    def _setup_ik_chain(self, start_jnt: pm.nt.Joint, end_jnt: pm.nt.Joint):
 
         # duplicate joints for ik chain
         ik_joint_name_fmt = "{0}_ik"
@@ -119,16 +154,17 @@ class SplineIKFKAction(BuildAction):
             cv_groups.append(cv)
         cv_groups.append(curve_shape.cv[-2:])
 
-        unused_mid_ik_ctls: List = self.midIkCtls
-        for i, cvs in enumerate(cv_groups):
+        unused_mid_ik_ctls: List = self.midIkCtls[:]
+        for i, cv in enumerate(cv_groups):
             if i == 0:
                 ik_ctl = self.startCtl
             elif i == len(cv_groups) - 1:
                 ik_ctl = self.endIkCtl
             else:
-                ik_ctl = self._get_closest_ctl_to_cv(cvs, unused_mid_ik_ctls)
+                cv_location = pm.dt.Vector(cv.getPosition())
+                ik_ctl = self._get_closest_ctl_to_location(cv_location, unused_mid_ik_ctls)
                 unused_mid_ik_ctls.remove(ik_ctl)
-            cluster, cluster_handle = pm.cluster(cvs, name=f"{ik_ctl.nodeName()}_spline_ik_cluster")
+            cluster, cluster_handle = pm.cluster(cv, name=f"{ik_ctl.nodeName()}_spline_ik_cluster")
             # rename shape to match cluster for debugging
             cluster_shape = cluster_handle.getShape(typ="clusterHandle")
             cluster_shape.rename(f"{cluster.nodeName()}HandleShape")
@@ -148,19 +184,47 @@ class SplineIKFKAction(BuildAction):
             delta_length >> self.endIkCtl.stretchDelta
             cmds.setAttr(f"{self.endIkCtl}.stretchDelta", edit=True, lock=True, channelBox=True)
 
-        # connect the target matrices to the joints
-        for ik_jnt, target_jnt in zip(ik_jnts[:-1], target_jnts[:-1]):
-            nodes.connect_matrix(ik_jnt.wm, target_jnt, nodes.ConnectMatrixMethod.SNAP)
-        nodes.connect_matrix(self.endIkCtl.wm, end_jnt, nodes.ConnectMatrixMethod.SNAP)
+        return ik_jnts
+
+    def _setup_ikfk_switch(
+        self, target_jnts: List[pm.nt.Joint], ik_leaders: List[pm.nt.Transform], fk_leaders: List[pm.nt.Transform]
+    ):
+        if len(target_jnts) != len(ik_leaders):
+            raise ValueError("ik_leaders is not the same length as target_jnts")
+        if len(target_jnts) != len(fk_leaders):
+            raise ValueError("fk_leaders is not the same length as target_jnts")
+
+        # add ikfk switch attr (integer, not blend)
+        self.startCtl.addAttr("ik", min=0, max=1, attributeType="short", defaultValue=1, keyable=1)
+        ik_attr = self.startCtl.attr("ik")
+
+        # create and connect ik / fk matrix choice nodes
+        for ik_leader, fk_leader, target_jnt in zip(ik_leaders, fk_leaders, target_jnts):
+            choice = util_nodes.choice(ik_attr, fk_leader.wm, ik_leader.wm)
+            choice.node().rename(f"{target_jnt.nodeName()}_ikfk_choice")
+            nodes.connect_matrix(choice, target_jnt, nodes.ConnectMatrixMethod.SNAP)
+
+        return ik_attr
 
     @staticmethod
-    def _get_closest_ctl_to_cv(cv: pm.NurbsCurveCV, ctls: List[pm.nt.Transform]) -> pm.nt.Transform:
+    def _get_closest_ctls(targets: List[pm.nt.Transform], ctls: List[pm.nt.Transform]) -> List[pm.nt.Transform]:
+        """
+        Return the closest control for each node in targets.
+        """
+        result = []
+        for target in targets:
+            location = target.getTranslation(space="world")
+            ctl = SplineIKFKAction._get_closest_ctl_to_location(location, ctls)
+            result.append(ctl)
+        return result
+
+    @staticmethod
+    def _get_closest_ctl_to_location(location: pm.dt.Vector, ctls: List[pm.nt.Transform]) -> pm.nt.Transform:
         best_ctl = None
         best_dist = None
-        cv_location = pm.dt.Vector(cv.getPosition())
         for ctl in ctls:
             ctl_location = ctl.getTranslation(space="world")
-            delta_location: pm.dt.Vector = (cv_location - ctl_location)
+            delta_location: pm.dt.Vector = location - ctl_location
             dist = delta_location.length()
             if best_dist is None or dist < best_dist:
                 best_dist = dist
