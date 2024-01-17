c1dfdc1f3194d2798e8dc38bae1d84469cf7c01a
Bohdon Sayre <bohdon@gmail.com>
2024-01-16 23:42:09 -0500

    add support for running tests on multiple maya versions

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,8 +1,12 @@
+import os
+import sys
 import unittest
 import maya.standalone
 
 
 def run_tests():
+    print("\n\n>>> Running pulse tests...")
+
     loader = unittest.TestLoader()
     start_dir = "tests"
     suite = loader.discover(start_dir)
